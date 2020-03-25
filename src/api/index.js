b6091787ade21c308fd7f91d6bdec031186c38c1
Álvaro Cuevas <arcs@gft.com>
2020-03-25 12:22:05 +0100

    minor changes

--- src/api/index.js
+++ src/api/index.js
@@ -2,30 +2,30 @@ import axios from 'axios';
 const API_URL = 'https://spanish-api-covid19.pedelriomarron.now.sh';
 
 const fetchSummary = async () => {
-  const { data } = await axios.get(`${API_URL}/api`);
-  return {
-    confirmed: data.confirmed,
-    recovered: data.recovered,
-    deaths: data.deaths,
-    hospitalized: data.hospitalized,
-    uci: data.uci,
-  };
+    const { data } = await axios.get(`${API_URL}/api`);
+    return {
+        confirmed: data.confirmed,
+        recovered: data.recovered,
+        deaths: data.deaths,
+        hospitalized: data.hospitalized,
+        uci: data.uci
+    };
 };
 
 const fetchRegions = async () => {
-  const { data } = await axios.get(`${API_URL}/api/regions`);
-  return data;
+    const { data } = await axios.get(`${API_URL}/api/regions`);
+    return data;
 };
 
 const fetchRegion = async region => {
-  const { data } = await axios.get(`${API_URL}/api/regions/${region}`);
-  return {
-    confirmed: data.confirmed,
-    recovered: data.recovered,
-    deaths: data.deaths,
-    hospitalized: data.hospitalized,
-    uci: data.uci,
-  };
+    const { data } = await axios.get(`${API_URL}/api/regions/${region}`);
+    return {
+        confirmed: data.confirmed,
+        recovered: data.recovered,
+        deaths: data.deaths,
+        hospitalized: data.hospitalized,
+        uci: data.uci
+    };
 };
 
 export { fetchSummary, fetchRegions, fetchRegion };
--- src/views/regions/regions.js
+++ src/views/regions/regions.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
+    BarChart,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    ResponsiveContainer
 } from 'recharts';
 import { fetchRegions, fetchRegion } from '../../api';
 import Spinner from '../../components/spinner';
@@ -14,126 +15,134 @@ import Spinner from '../../components/spinner';
 import './regions.scss';
 
 const RegionsView = () => {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [regions, setRegions] = useState([]);
-  const [selectedRegion, setSelectedRegion] = useState('10');
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [regions, setRegions] = useState([]);
+    const [selectedRegion, setSelectedRegion] = useState('10');
 
-  useEffect(() => {
-    const fetchOptions = async () => {
-      try {
-        const res = await fetchRegions();
-        const { regions } = res;
+    useEffect(() => {
+        const fetchOptions = async () => {
+            try {
+                const res = await fetchRegions();
+                const { regions } = res;
 
-        let regionsArr = [];
-        Object.entries(regions).forEach(reg =>
-          regionsArr.push({ id: reg[0], value: reg[1] })
+                let regionsArr = [];
+                Object.values(regions).forEach(reg => {
+                    // let test = { id: reg[0], value: reg[1] };
+                    regionsArr.push(reg);
+                });
+                setRegions(regionsArr);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchOptions();
+    }, []);
+
+    useEffect(() => {
+        const fetchRegionData = async region => {
+            setLoading(true);
+            try {
+                const res = await fetchRegion(region);
+                setData(res);
+            } catch (err) {
+                setError(err);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchRegionData(selectedRegion);
+    }, [selectedRegion]);
+
+    const renderRegions = () => (
+        <select onChange={e => setSelectedRegion(e.target.value)}>
+            {regions.map(region => (
+                <option key={region.code} value={region.code}>
+                    {region.name}
+                </option>
+            ))}
+        </select>
+    );
+
+    const renderGraph = () => {
+        const { confirmed, recovered, deaths, hospitalized, uci } = data;
+
+        const plot = [
+            {
+                name: 'Confirmados',
+                totales: confirmed.value,
+                estimados: confirmed.estimateTomorrow
+            },
+            {
+                name: 'Recuperados',
+                totales: recovered.value,
+                estimados: recovered.estimateTomorrow
+            },
+            {
+                name: 'Muertos',
+                totales: deaths.value,
+                estimados: deaths.estimateTomorrow
+            },
+            {
+                name: 'Hospitalizados',
+                totales: hospitalized.value,
+                estimados: hospitalized.estimateTomorrow
+            },
+            { name: 'UCI', totales: uci.value, estimados: uci.estimateTomorrow }
+        ];
+
+        return (
+            <ResponsiveContainer>
+                <BarChart
+                    width={800}
+                    height={350}
+                    data={plot}
+                    margin={{
+                        top: 20
+                    }}>
+                    <CartesianGrid strokeDasharray='3 3' />
+                    <XAxis dataKey='name' />
+                    <YAxis
+                        domain={[0, 'auto']}
+                        yAxisId='left'
+                        orientation='left'
+                        stroke='#8884d8'
+                    />
+                    <YAxis
+                        yAxisId='right'
+                        orientation='right'
+                        stroke='#82ca9d'
+                    />
+                    <Tooltip />
+                    <Legend />
+                    <Bar yAxisId='left' dataKey='totales' fill='#8884d8' />
+                    <Bar yAxisId='right' dataKey='estimados' fill='#82ca9d' />
+                </BarChart>
+            </ResponsiveContainer>
         );
-        setRegions(regionsArr);
-      } catch (err) {
-        setError(err);
-      } finally {
-        setLoading(false);
-      }
     };
 
-    fetchOptions();
-  }, []);
-
-  useEffect(() => {
-    fetchRegionData(selectedRegion);
-  }, [selectedRegion]);
-
-  const fetchRegionData = async region => {
-    setLoading(true);
-    try {
-      if (region.length !== 0) {
-        const res = await fetchRegion(region);
-        res && setData(res);
-      }
-    } catch (err) {
-      setError(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const renderRegions = () => {
-    const { confirmed, recovered, deaths, hospitalized, uci } = data;
-
-    const plot = [
-      {
-        name: 'Confirmados',
-        totales: confirmed.value,
-        estimados: confirmed.estimateTomorrow,
-      },
-      {
-        name: 'Recuperados',
-        totales: recovered.value,
-        estimados: recovered.estimateTomorrow,
-      },
-      {
-        name: 'Muertos',
-        totales: deaths.value,
-        estimados: deaths.estimateTomorrow,
-      },
-      {
-        name: 'Hospitalizados',
-        totales: hospitalized.value,
-        estimados: hospitalized.estimateTomorrow,
-      },
-      { name: 'UCI', totales: uci.value, estimados: uci.estimateTomorrow },
-    ];
+    const renderSpinner = () => (
+        <div className='loader'>
+            <Spinner />
+        </div>
+    );
 
     return (
-      <BarChart
-        width={800}
-        height={350}
-        data={plot}
-        margin={{
-          top: 20,
-        }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis
-          domain={[0, 'auto']}
-          yAxisId="left"
-          orientation="left"
-          stroke="#8884d8"
-        />
-        <YAxis yAxisId="right" orientation="right" stroke="#82ca9d" />
-        <Tooltip />
-        <Legend />
-        <Bar yAxisId="left" dataKey="totales" fill="#8884d8" />
-        <Bar yAxisId="right" dataKey="estimados" fill="#82ca9d" />
-      </BarChart>
+        <>
+            <div className='regions'>
+                {renderRegions()}
+                {loading && renderSpinner()}
+                {!loading && data && renderGraph()}
+                {error && 'Error al obtener los datos'}
+            </div>
+        </>
     );
-  };
-
-  const renderSpinner = () => (
-    <div className="loader">
-      <Spinner />
-    </div>
-  );
-
-  return (
-    <>
-      <div className="regions">
-        <select onChange={e => setSelectedRegion(e.target.value)}>
-          {regions.map(region => (
-            <option key={region.id} value={region.id}>
-              {region.value}
-            </option>
-          ))}
-        </select>
-        {loading && renderSpinner()}
-        {!loading && data && renderRegions()}
-        {error && 'Error al obtener los datos'}
-      </div>
-    </>
-  );
 };
 
 export default RegionsView;
